9e4d9c7196fe712bb98b87029785aba8b2ff6280
Aimtrue345 <little.rickpollard@gmail.com>
2025-07-09 22:24:17 -0400

    Completed bookbot, a boot.dev project.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def main():
-    from stats import count_words, count_characters
+    import sys
+    from stats import count_words, count_characters, sort_char
 
-    count_words("books/frankenstein.txt")
-    char_count = count_characters("books/frankenstein.txt")
-    print(char_count)
-
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        count_words(book_path)
+        char_count = count_characters(book_path)
+        sort_char(char_count)
+        print("============= END ===============")
+    
 
 main()
--- stats.py
+++ stats.py
@@ -6,7 +6,8 @@ def count_words(book_path):
         words = book_contents.split()
     for word in words:
         num_words = num_words + 1
-    print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
 
 # Reads text file, counts through each character in text and appends to dictionary.
 def count_characters(book_path):
@@ -17,6 +18,25 @@ def count_characters(book_path):
         char_count[char] = char_count.get(char, 0) + 1
     return char_count
 
+# Takes a dictionary and returns the value of the "num" key.
 
+def sort_num(list):
+    return list["num"]
+
+# Sort char_count from highest to lowest and filter non-alpha characters.
+
+def sort_char(char_count):
+    count_list = []
+    for char in char_count:
+        count_dict = {}
+        num = char_count[char]
+        count_dict["char"] = char
+        count_dict["num"] = num
+        count_list.append(count_dict)
+    count_list.sort(reverse=True, key=sort_num)
+    print("--------- Character Count -------")
+    for count_dict in count_list:
+        if count_dict["char"].isalpha() == True:
+            print(f"{count_dict["char"]}: {count_dict["num"]}")
 
         
